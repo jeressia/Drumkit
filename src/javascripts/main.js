3658eea8bc57ac170afba1df954aae71508f2ab2
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-10-02 18:38:21 -0500

    keys play music, wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,36 @@
 import 'bootstrap';
 import '../styles/main.scss';
+
+const playSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.keyboard-key[data-key="${e.keyCode}"]`);
+  console.error(key);
+  key.classList.add('pressed');
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const keyListener = () => {
+  window.addEventListener('keydown', playSound);
+};
+
+const removeTransition = () => {
+  console.error('I am working');
+  this.classList.remove('pressed');
+};
+
+const removeListener = () => {
+  const allKeys = document.querySelectorAll('.keyboard-key');
+  allKeys.forEach((key) => {
+    key.addEventListener('transitionend', removeTransition);
+  });
+};
+
+
+const init = () => {
+  keyListener();
+  removeListener();
+};
+
+init();
